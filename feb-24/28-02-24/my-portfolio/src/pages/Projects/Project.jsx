107d6defc7acf49d80cd779dc8499b80afdabbfb
banchet18 <banjarac6@gmail.com>
2024-02-26 11:53:59 +0530

    c

--- feb-24/28-02-24/my-portfolio/src/pages/Projects/Project.jsx
+++ feb-24/28-02-24/my-portfolio/src/pages/Projects/Project.jsx
@@ -1,13 +1,55 @@
 import "./Project.css";
+import { FaGithub } from "react-icons/fa";
 const Project = () => {
   return (
-    <div className="projectPage">
-      <div className="card first">weather app</div>
-      <div className="card">hello</div>
-      <div className="card">hello</div>
-      <div className="card">hello</div>
-      <div className="card">hello</div>
-    </div>
+    <>
+      <div className="projectPage">
+        <div className="card first">
+          <div>
+            <img
+              src="https://t4.ftcdn.net/jpg/04/97/80/99/360_F_497809944_FMo3DO6j7XSlb9rZKOlnqaaWoJhuZXBm.jpg"
+              alt=""
+            />
+          </div>
+          <div className="weatherName">
+            Weather App
+            <div className="projectsicon">
+              <span>
+                <FaGithub />
+              </span>
+              <span>Live</span>
+            </div>
+          </div>
+        </div>
+        <div className="card first">
+          <div>
+            <img
+              src="https://t4.ftcdn.net/jpg/04/97/80/99/360_F_497809944_FMo3DO6j7XSlb9rZKOlnqaaWoJhuZXBm.jpg"
+              alt=""
+            />
+          </div>
+          <div></div>
+        </div>
+        <div className="card third">
+          <div>
+            <img
+              src="https://t4.ftcdn.net/jpg/04/97/80/99/360_F_497809944_FMo3DO6j7XSlb9rZKOlnqaaWoJhuZXBm.jpg"
+              alt=""
+            />
+          </div>
+          <div></div>
+        </div>
+        <div className="card third">
+          <div>
+            <img
+              src="https://t4.ftcdn.net/jpg/04/97/80/99/360_F_497809944_FMo3DO6j7XSlb9rZKOlnqaaWoJhuZXBm.jpg"
+              alt=""
+            />
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </>
   );
 };
 export default Project;
