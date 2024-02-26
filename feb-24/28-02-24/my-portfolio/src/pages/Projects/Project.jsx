57bd6bddd4b4de96f1fe4c87bfce24f588b58830
banchet18 <banjarac6@gmail.com>
2024-02-26 10:20:01 +0530

    c

--- feb-24/28-02-24/my-portfolio/src/pages/Projects/Project.jsx
+++ feb-24/28-02-24/my-portfolio/src/pages/Projects/Project.jsx
@@ -2,7 +2,7 @@ import "./Project.css";
 const Project = () => {
   return (
     <div className="projectPage">
-      <div className="card">hello</div>
+      <div className="card first">weather app</div>
       <div className="card">hello</div>
       <div className="card">hello</div>
       <div className="card">hello</div>
