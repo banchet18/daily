ba00053939bef744b06816202f2db8b298b9e39a
banchet18 <banjarac6@gmail.com>
2024-02-26 12:37:41 +0530

    c

---
+++ feb-24/28-02-24/my-portfolio/src/utils.jsx
@@ -0,0 +1,18 @@
+const projects = [
+  {
+    id: 1,
+    img: "https://t4.ftcdn.net/jpg/04/97/80/99/360_F_497809944_FMo3DO6j7XSlb9rZKOlnqaaWoJhuZXBm.jpg",
+    projectName: "react",
+    git: "https://github.com/banchet18/reacts-projects/tree/main/redux-project",
+    demo: "blank",
+  },
+  {
+    id: 2,
+    img: "https://t4.ftcdn.net/jpg/04/97/80/99/360_F_497809944_FMo3DO6j7XSlb9rZKOlnqaaWoJhuZXBm.jpg",
+    projectName: "weather",
+    git: "https://github.com/banchet18/reacts-projects/tree/main/redux-project",
+    demo: "https://reduxaddtocart.netlify.app/",
+  },
+];
+
+export default projects;
