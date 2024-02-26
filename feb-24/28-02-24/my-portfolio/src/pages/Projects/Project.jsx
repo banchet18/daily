912a43a183bc7664c3c3b73e6a33c2b7da7bb980
banchet18 <banjarac6@gmail.com>
2024-02-26 10:14:49 +0530

    c

--- feb-24/28-02-24/my-portfolio/src/pages/Projects/Project.jsx
+++ feb-24/28-02-24/my-portfolio/src/pages/Projects/Project.jsx
@@ -1,12 +1,12 @@
 import "./Project.css";
 const Project = () => {
   return (
-    <div className="aboutPage">
-      <div>hello</div>
-      <div>hello</div>
-      <div>hello</div>
-      <div>hello</div>
-      <div>hello</div>
+    <div className="projectPage">
+      <div className="card">hello</div>
+      <div className="card">hello</div>
+      <div className="card">hello</div>
+      <div className="card">hello</div>
+      <div className="card">hello</div>
     </div>
   );
 };
