d062db64d2a4a9ba6fdebcd0d501c7318bbdaecb
banchet18 <banjarac6@gmail.com>
2024-02-26 13:06:34 +0530

    c

--- feb-24/28-02-24/my-portfolio/src/utils.jsx
+++ feb-24/28-02-24/my-portfolio/src/utils.jsx
@@ -2,14 +2,14 @@ const Projects = [
   {
     id: 1,
     img: "https://t4.ftcdn.net/jpg/04/97/80/99/360_F_497809944_FMo3DO6j7XSlb9rZKOlnqaaWoJhuZXBm.jpg",
-    projectName: "redux app",
+    projectName: "Redux app",
     git: "https://github.com/banchet18/reacts-projects/tree/main/redux-project",
     demo: "https://reduxaddtocart.netlify.app/",
   },
   {
     id: 2,
     img: "https://t4.ftcdn.net/jpg/04/97/80/99/360_F_497809944_FMo3DO6j7XSlb9rZKOlnqaaWoJhuZXBm.jpg",
-    projectName: "weather app",
+    projectName: "Weather app",
     git: "https://github.com/banchet18/reacts-projects/tree/main/redux-project",
     demo: "https://reduxaddtocart.netlify.app/",
   },
