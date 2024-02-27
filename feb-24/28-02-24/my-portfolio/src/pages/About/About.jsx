8e2b800d8fb7ad007a7859f3c322d559d4447336
banchet18 <banjarac6@gmail.com>
2024-02-27 11:45:01 +0530

    c

--- feb-24/28-02-24/my-portfolio/src/pages/About/About.jsx
+++ feb-24/28-02-24/my-portfolio/src/pages/About/About.jsx
@@ -1,6 +1,21 @@
 import "./About.css";
 
 const About = () => {
-  return <div className="aboutDiv">About</div>;
+  return (
+    <div className="aboutDiv">
+      <div className="box1">
+        <span className="box1span">About Me</span>
+      </div>
+      <div className="box2">
+        <div className="box2first">
+          <img
+            src="https://getillustrations.b-cdn.net//photos/pack/3d-avatar-male_lg.png"
+            alt=""
+          />
+        </div>
+        <div className="box2second">ChetanKumar</div>
+      </div>
+    </div>
+  );
 };
 export default About;
