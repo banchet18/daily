85dafe1682e5967cea8ac41b7b38f8fb9ed3dd7e
banchet18 <banjarac6@gmail.com>
2024-03-10 08:27:05 +0530

    c

--- feb-24/28-02-24/my-portfolio/src/pages/About/About.jsx
+++ feb-24/28-02-24/my-portfolio/src/pages/About/About.jsx
@@ -13,7 +13,18 @@ const About = () => {
             alt=""
           />
         </div>
-        <div className="box2second">ChetanKumar</div>
+        <div className="box2second">
+          Hey, I'm ChetanKumar Banjara, a frontend developer who's all about
+          crafting visually appealing and user-friendly websites. With a knack
+          for turning ideas into pixel-perfect designs using HTML, CSS, and
+          JavaScript, I create seamless online experiences that work across
+          devices. Always eager to explore the latest tech trends, I bring a
+          dynamic and collaborative spirit to the table, making teamwork an
+          essential part of my coding journey. With 3 Month in the field, I'm
+          not just a coder; I'm a digital storyteller passionate about
+          transforming visions into captivating online realities. Let's connect
+          and bring your digital projects to life!
+        </div>
       </div>
     </div>
   );
