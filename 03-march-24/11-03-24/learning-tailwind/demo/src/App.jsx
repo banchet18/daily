287691e579a9285cc2259309cf3ca4a8ca6374e5
banchet18 <banjarac6@gmail.com>
2024-03-11 10:44:44 +0530

    c

--- 03-march-24/11-03-24/learning-tailwind/demo/src/App.jsx
+++ 03-march-24/11-03-24/learning-tailwind/demo/src/App.jsx
@@ -1,4 +1,8 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="flex justify-center items-center h-screen bg-slate-500">
+      <h1 className=" text-8xl font-bold text-pink-400">I'm Tailwind</h1>
+    </div>
+  );
 };
 export default App;
